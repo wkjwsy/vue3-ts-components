14c4b5c5959ea27b38df7ae89b30151ce75ac219
wkjwsy <1198622318@qq.com>
2021-06-09 00:07:01 +0800

    添加格式化文件

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   transform: {
-    "^.+\\.vue$": "vue-jest",
+    '^.+\\.vue$': 'vue-jest', // 通过vue-jest去编译.vue文件为js，让测试用例可以跑起来
   },
-};
+}
