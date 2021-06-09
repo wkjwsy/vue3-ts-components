3290bccb888fc02102f28477f27ef29203c873e9
wkjwsy <1198622318@qq.com>
2021-06-09 23:08:12 +0800

    用defineComponent改写入口App.vue

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
+import { createApp, defineComponent, h } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+const img = require('./assets/logo.png') // eslint-disable-line
+const App = defineComponent({
+  render() {
+    return h('div', { id: 'app' }, [
+      h('img', {
+        alt: 'Vue logo',
+        src: img,
+      }),
+      h(HelloWorld, {
+        msg: 'wkj test',
+      }),
+    ])
+  },
+})
 createApp(App).mount('#app')
